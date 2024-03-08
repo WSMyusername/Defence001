8e189c1297e91237dba122760b27837f87c673c8
dhekdj12 <142203977+dhekdj12@users.noreply.github.com>
2024-03-08 10:52:44 +0900

    1234

---
+++ Defence001/Assets/KJ/Scripts/ButtonMng.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+
+
+
+public class ButtonMng : MonoBehaviour
+{
+    public void GameStartBtn()
+    {
+
+    }
+}
